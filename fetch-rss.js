e675ba32c33c492e34ac6ea1401929fa8eb41773
Mike van Rossum <mike@askmike.org>
2012-09-01 01:06:06 +0200

    simplified script: now using build in event listener

--- fetch-rss.js
+++ fetch-rss.js
@@ -2,41 +2,34 @@ var FeedParser = require('feedparser')
 ,   parser = new FeedParser()
 ,   sql = require('/config/CMDA-tweets/sql.js')
 ,   l = console.log
-,   hour = 1000 * 60 * 60
-// ,   hour = 1000
+,   interval = 1000 * 60 * 30 // 30 min
+// ,   interval = 1000
 ,   fetch = function() {
-    l('fetching at ' + new Date());
-
+    l('new fetch at ' + new Date());
     [
         'http://intra.iam.hva.nl/content/rss-feeds/med-rss.xml'
     ,   'http://intra.iam.hva.nl/content/rss-feeds/gg-rss.xml'
     ,   'http://intra.iam.hva.nl/content/rss-feeds/uit-rss.xml'
-    ].forEach(function(url, i) {
-        
-        parser.parseFile(url, function(error, meta, articles){
-            if (error) l(error);
-            else {
-                var ts;
-                articles.forEach(function(article) {
-                    ts = Math.round( Date.parse( article.date ) / 1000 );
-
-                    if(i === 0) { // only articles from 'med' have authors
-                        sql.query(
-                            "INSERT IGNORE INTO `intranet` (`timestamp` ,`author` ,`title` ,`content`, `stream`) VALUES (?, ?, ?, ?, ?);"
-                        ,   [ ts, article['rss:author']['#'], article.title, article.description, url ]
-                        );   
-                    } else {
-                        sql.query(
-                            "INSERT IGNORE INTO `intranet` (`timestamp`, `title` ,`content`, `stream`) VALUES (?, ?, ?, ?);"
-                        ,   [ ts, article.title, article.description, url ]
-                        );
-                    }
-                });
-               setTimeout( fetch, hour )
-            }
-        });
-
-    });
+    ].forEach(function(url) {
+        parser.parseFile(url);
+    });   
 }
+,   ts
+,   storeArticle = function( article ) {
+    ts = Math.round( Date.parse( article.date ) / 1000 );
+    if(article['rss:author']) { // if we have an author, store it to
+        sql.query(
+            "INSERT IGNORE INTO `intranet` (`timestamp` ,`author` ,`title` ,`content`, `url`, `stream`) VALUES (?, ?, ?, ?, ?, ?);"
+        ,   [ ts, article['rss:author']['#'], article.title, article.description, article.link, article.meta.xmlUrl ]
+        );   
+    } else {
+        sql.query(
+            "INSERT IGNORE INTO `intranet` (`timestamp`, `title` ,`content`, `url`, `stream`) VALUES (?, ?, ?, ?, ?);"
+        ,   [ ts, article.title, article.description, article.link, article.meta.xmlUrl ]
+        );
+    }
+}
+parser.on('article', storeArticle);
 
 fetch();
+setInterval( fetch, interval );
